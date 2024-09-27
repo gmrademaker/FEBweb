d687c040494b50509c174e0ae088348bee9900c5
gmrademaker <145447388+gmrademaker@users.noreply.github.com>
2024-09-27 14:15:19 +0200

    hamburger menu werkt

--- script/script.js
+++ script/script.js
@@ -4,5 +4,8 @@ openButton.onclick = openMenu;
  
 function openMenu() {
   var deNav = document.querySelector("nav");
+  var noScroll = document.querySelector("body");
+
   deNav.classList.toggle("toonMenu");
+  noScroll.classList.toggle("toonMenu");
 }
